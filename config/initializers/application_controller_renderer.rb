892755358bff0dbc30ff1550e665e15e20f1499a
mamiyamahara <y.mami0324@gmail.com>
2020-08-12 23:08:51 +0900

    [Add] 画像投稿機能

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '153637a7e7360f9a761c817643513f76e1833347d28ffc3101b18bd54e8e057d37077d26ff09185e9be4a4a94fcb9f5866435021ed834ebc5226294145e4eaf9'
